f3e793912fffb6a61f39603e58f224c1a565fa1f
Sadik Yusuf Adam <yusufadamsadiq7@gmail.com>
2026-08-06 01:49:11 +0100

    Add optional port argument

--- ghostscan.py
+++ ghostscan.py
@@ -4,7 +4,13 @@ import argparse
 
 
 def main():
-    print("Welcome to GhostScan!")
+    parser = argparse.ArgumentParser(description="GhostScan - A Python-based port scanner")
+    parser.add_argument("target", help="Target URL or IP address to scan")
+    parser.add_argument("-p","--port",help="Port number to scan", default="Default")
+    args = parser.parse_args()
+    print(f"Target: {args.target}")
+    print(f"Port: {args.port}")
+
 
 
 if __name__ == "__main__":
